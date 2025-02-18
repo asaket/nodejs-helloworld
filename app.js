fdcc829d1cdcf8042094fe1867cabc734aeec108
asaket <ashish.saket@gmail.com>
2025-02-18 04:17:16 -0500

    adding sleep in the code to test HPA in OpenShift

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const http = require('http');
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const server = http.createServer(async (req, res) => {
   const clientIP = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   const timestamp = new Date().toISOString();
   
   console.log(`[${timestamp}] Request received from IP: ${clientIP}, Method: ${req.method}, URL: ${req.url}`);
+
+  // Add milliseconds of sleep
+  await sleep(5000);
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
